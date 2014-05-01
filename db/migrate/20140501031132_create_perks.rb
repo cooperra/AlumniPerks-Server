4a8be9cd2bcf5e406a6d0734139e1a2cf6a6a869
Robbie Cooper <cooperra@rose-hulman.edu>
2014-05-01 00:12:07 -0400

    Add is_deleted and image_updated_at to perk

--- db/migrate/20140501031132_create_perks.rb
+++ db/migrate/20140501031132_create_perks.rb
@@ -1,13 +1,15 @@
 class CreatePerks < ActiveRecord::Migration
   def change
     create_table :perks do |t|
-      t.primary_key :id
+      #t.primary_key :id
       t.string :company_name
       t.string :company_address
       t.string :company_phone
       t.string :description
       t.string :website
       t.string :coupon
+      t.boolean :is_deleted
+      t.timestamp :image_updated_at
 
       t.timestamps
     end
