4bef5f5d816de239503357b15e675673d67d133b
Robbie Cooper <cooperra@rose-hulman.edu>
2014-05-18 22:44:59 -0400

    Change perk image storage location

--- app/models/perk.rb
+++ app/models/perk.rb
@@ -27,7 +27,7 @@ class Perk < ActiveRecord::Base
   has_attached_file :image, :styles => {
     :medium => "300x300",
     :thumb => "100x100"
-  }#, :default_url => "/images/:style/missing.png"
+  }, :path => ':rails_root/public/system/:class/:attachment/:style/:id.:extension'#, :default_url => "/images/:style/missing.png"
   # omit default url because we want 404 for API
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
